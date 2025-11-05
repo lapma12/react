3cfa3f0de0b82f3cb5d7f63a7663bb42ecf6a956
Lapma <lapostyanm@kkszki.hu>
2025-11-05 11:58:03 +0100

    Navbar new style and structure is ready to use.

--- frontend/src/Component/Navbar.js
+++ frontend/src/Component/Navbar.js
@@ -1,7 +1,16 @@
 import { useRef, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import "../Styles/Navbar.css";
 import { NavLink } from "react-router-dom";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+import { FaRegMessage } from "react-icons/fa6";
+import { IoMdHome } from "react-icons/io";
+import { BsTable } from "react-icons/bs";
+import { MdAccountCircle } from "react-icons/md";
+import { IoLogoGameControllerB } from "react-icons/io";
+import { IoSearchSharp } from "react-icons/io5";
+import { RiTwitterXFill } from "react-icons/ri";
+import "../Styles/Navbar.css";
 
 function Navbar() {
   const navRef = useRef();
@@ -16,36 +25,32 @@ function Navbar() {
     navRef.current.classList.remove("responsive_nav");
     setMenuOpen(false);
   };
-  
+
   return (
     <header className="navbar">
-      <div className="left">
-        <h3>CastL</h3>
+      <div className="navbar-left">
+        <button onClick={() => window.open("https://www.instagram.com", "_blank")}><FaInstagram /></button>
+        <button onClick={() => window.open("https://www.facebook.com", "_blank")}><AiOutlineFacebook /></button>
+        <button onClick={() => window.open("https://twitter.com", "_blank")}><RiTwitterXFill /></button>
       </div>
-      <nav ref={navRef} className="center">
-        <NavLink className={ ({isActive}) =>
-          isActive ? "active" : ""} to="/" onClick={closeNavbar}>
-          Home
-        </NavLink>
-        <NavLink className={ ({isActive}) =>
-          isActive ? "active" : ""} to="/about" onClick={closeNavbar}>
-          About
-        </NavLink>
-        <NavLink className={ ({isActive}) =>
-          isActive ? "active" : ""} to="/scoreboard" onClick={closeNavbar}>
-          ScoreBoard
-        </NavLink>
-        <NavLink className={ ({isActive}) =>
-          isActive ? "active" : ""} to="/game" onClick={closeNavbar}>
-          Game
-        </NavLink>
-        <NavLink className={ ({isActive}) =>
-          isActive ? "active" : ""} to="/login" onClick={closeNavbar}>
-          Sign in
-        </NavLink>
+
+      <nav ref={navRef} className="navbar-center">
+        <NavLink to="/" onClick={closeNavbar}><span className="icon"><IoMdHome /></span>Home</NavLink>
+        <NavLink to="/about" onClick={closeNavbar}><span className="icon"><FaRegMessage /></span>About</NavLink>
+        <NavLink to="/scoreboard" onClick={closeNavbar}><span className="icon"><BsTable /></span>Scoreboard</NavLink>
+        <NavLink to="/game" onClick={closeNavbar}><span className="icon"><IoLogoGameControllerB />
+        </span>Game</NavLink>
+        <NavLink to="/login" onClick={closeNavbar}><span className="icon"><MdAccountCircle /></span>Sign In</NavLink>
+
+        <div className="nav-search-mobile">
+          <button className="search-btn"><IoSearchSharp /></button>
+        </div>
       </nav>
-      <div className="right">
-        <button className="nav-btn" onClick={toggleNavbar}>
+
+      <div className="navbar-right">
+        <button className="search-btn desktop-only"><IoSearchSharp /></button>
+
+        <button className="nav-toggle-btn" onClick={toggleNavbar}>
           {menuOpen ? <FaTimes /> : <FaBars />}
         </button>
       </div>

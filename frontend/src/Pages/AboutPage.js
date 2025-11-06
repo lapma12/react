6334e4c0be95aa3cd17924e0cd8c39c52a1f362b
lapma12 <lapostyanm@kkszki.hu>
2025-11-06 16:41:58 +0100

    About page new style and structure is ready to use.

--- frontend/src/Pages/AboutPage.js
+++ frontend/src/Pages/AboutPage.js
@@ -12,7 +12,7 @@ const AboutPage = () => {
       name: "Lapma",
       role: "Website Developer",
       bio: "Responsible for the animations of the game characters. Brings movements and characters to life.",
-      image: 'https://avatars.githubusercontent.com/u/112930985?v=4'
+      image: 'https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-1/455037356_1227391901621943_2585659180847469402_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=105&ccb=1-7&_nc_sid=e99d92&_nc_ohc=k6Z-Si2nGqQQ7kNvwGlgoF3&_nc_oc=Adns4iJpYGfU-zL4RlfznBOs1HYzUOjxlSaJkp49IPlP49InHVmEZ5mciV2Q8Ajf9zM0_VqwNMD8MVTuSRpJ3yzr&_nc_zt=24&_nc_ht=scontent-vie1-1.xx&_nc_gid=0umAl1PnOfAB6qrBJaPiLA&oh=00_AfiEPfe6bi_a3vFTdobrQInqANEJeSkkPR4zpnBK5MoS0Q&oe=69128EBD'
     }
   ];
 
@@ -24,7 +24,12 @@ const AboutPage = () => {
           Welcome to the website of our 3D game development team! Our goal is to create an impressive 3D game that offers a unique experience for players. Our team is committed to quality and innovation.
         </p>
       </div>
-
+      <div className="about-section">
+        <h2>About Our Game</h2>
+        <div className="about-content">
+          <p> Our game takes place in a stunning 3D world where players embark on unique adventures. We're developing the game using modern graphics solutions and innovative gameplay mechanics. </p> <p> Throughout the project, we utilize the latest 3D modeling and animation techniques to create a realistic and captivating world. </p> <p> We believe that quality and player experience are the most important aspects, which is why we pay close attention to every detail during development. </p>
+        </div>
+      </div>
       <div className="team-section">
         <h2>Our Team</h2>
         <div className="team-grid">
@@ -42,13 +47,6 @@ const AboutPage = () => {
               <p className="team-member-bio">{member.bio}</p>
             </div>
           ))}
-        </div>
-      </div>
-
-      <div className="about-section">
-        <h2>About Our Game</h2>
-        <div className="about-content">
-          <p> Our game takes place in a stunning 3D world where players embark on unique adventures. We're developing the game using modern graphics solutions and innovative gameplay mechanics. </p> <p> Throughout the project, we utilize the latest 3D modeling and animation techniques to create a realistic and captivating world. </p> <p> We believe that quality and player experience are the most important aspects, which is why we pay close attention to every detail during development. </p>
         </div>
       </div>
     </div>
